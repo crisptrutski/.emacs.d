e28ff1edc48ea897e678b1864761aa0dc2ce3c7a
Chris Truter <jeffpalentine@gmail.com>
2015-01-29 15:14:06 +0200

    Shortcuts, themes + indent settings

--- init.el
+++ init.el
@@ -15,7 +15,6 @@
                       starter-kit-lisp
                       ;; Clojure & friends
                       clojure-mode
-                      clojure-test-mode
                       cider
                       ;;expectations-mode (brings in nrepl, woes)
                       popup
@@ -63,6 +62,20 @@
 
 (define-clojure-indent
   (defroutes 'defun)
+  (for-map 'defun)
+  (nextTuple 'defun)
+  (ack 'defun)
+  (fail 'defun)
+
+  ;; om stuff
+  (div 'defun)
+  (ul 'defun)
+  (li 'defun)
+  (button 'defun)
+
+  ;; build-ins
+  (add-watch 'defun)
+
   (GET 2)
   (POST 2)
   (PUT 2)
@@ -77,3 +90,50 @@
 (defalias 'redo 'undo-tree-redo)
 (global-set-key (kbd "C-/") 'undo)
 (global-set-key (kbd "M-/") 'redo)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector (vector "#cccccc" "#f2777a" "#99cc99" "#ffcc66" "#6699cc" "#cc99cc" "#66cccc" "#2d2d2d"))
+ '(auto-save-default nil)
+ '(backup-inhibited t t)
+ '(cider-buffer-name-show-port t)
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
+ '(custom-safe-themes (quote ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
+ '(delete-selection-mode t)
+ '(fci-rule-color "#393939")
+ '(magit-emacsclient-executable "/Applications/Emacs.app/Contents/MacOS/bin/emacsclient")
+ '(nrepl-host "localhost")
+ '(nrepl-port "9991")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map (quote ((20 . "#f2777a") (40 . "#f99157") (60 . "#ffcc66") (80 . "#99cc99") (100 . "#66cccc") (120 . "#6699cc") (140 . "#cc99cc") (160 . "#f2777a") (180 . "#f99157") (200 . "#ffcc66") (220 . "#99cc99") (240 . "#66cccc") (260 . "#6699cc") (280 . "#cc99cc") (300 . "#f2777a") (320 . "#f99157") (340 . "#ffcc66") (360 . "#99cc99"))))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-warning-face ((((class color) (min-colors 89)) (:weight bold :foreground "#cc0000"))))
+ '(idle-highlight ((t (:inherit region :background "maroon4"))))
+ '(linum-highlight-face ((t (:inherit default :background "color-238" :foreground "white"))) t)
+ '(show-paren-match ((((class color) (min-colors 89)) (:background "#d7d7ff")))))
+
+
+(set-face-attribute 'default t :font "Menlo Regular-12")
+
+;; (set-frame-parameter (selected-frame) 'alpha '(97 50))
+;; (add-to-list 'default-frame-alist '(alpha 97 50))
+
+;; 'My workflow reloaded' / clojure
+(defun nrepl-reset ()
+  (interactive)
+  (set-buffer "*cider-repl dev*")
+  (goto-char (point-max))
+  (insert "(user/reset)")
+  (cider-repl-return))
+
+
+(global-set-key (kbd "<M-f5>") 'nrepl-reset)
